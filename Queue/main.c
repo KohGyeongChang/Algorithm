ecfab398178efa8d33214da7f04873735871c0ec
KohGyeongChang <imbrian@icloud.com>
2017-11-13 01:34:51 +0900

    library creation added

--- Queue/main.c
+++ Queue/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "queue.h"
+#include "./lib/queue.h"
 
 extern nodeData* createNodeData();
 extern queueLinkedList* createNode(char* data);
