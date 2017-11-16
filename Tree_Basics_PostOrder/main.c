4626155cc7aad95b8537a65234f05f022b81573c
KohGyeongChang <imbrian@icloud.com>
2017-11-16 11:43:49 +0900

    Tree Common Library Added

--- Tree_Basics_PostOrder/main.c
+++ Tree_Basics_PostOrder/main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "./lib/tree_postorder.h"
 
 extern void initializeTree();
 extern void makeTree();
 extern int traverseTree_PostOrder();
-extern void showNodeData(treeNode*);
 
 int main()
 {
