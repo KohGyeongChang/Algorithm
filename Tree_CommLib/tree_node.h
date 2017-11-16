e7f1ffcb230ded6f71f4976bb929904675382541
KohGyeongChang <imbrian@icloud.com>
2017-11-16 22:54:09 +0900

    Code For AVL Tree Added

--- Tree_CommLib/tree_node.h
+++ Tree_CommLib/tree_node.h
@@ -1,12 +1,14 @@
-#ifndef TREE_PREORDER_H
-#define TREE_PREORDER_H
+#ifndef TREE_NODE_H
+#define TREE_NODE_H
 
 typedef struct TREENODEDATA {
 	char data;
+	int nData;
 }treeNodeData;
 
 typedef struct TREENODE {
 	treeNodeData* pData;
+	int balance;
 	struct TREENODE* left;
 	struct TREENODE* right;
 } treeNode;
