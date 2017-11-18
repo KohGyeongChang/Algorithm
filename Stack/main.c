212650fb7c3fbe3dba06a9ace9417c097b312df6
KohGyeongChang <imbrian@icloud.com>
2017-11-18 11:30:56 +0900

    Clean Code Of Stack's main.c

--- Stack/main.c
+++ Stack/main.c
@@ -3,15 +3,8 @@
 #include <string.h>
 #include "./lib/stack.h"
 
-extern nodeData* createNodeData();
-extern stackNode* createNode(char* data);
-extern stackNode* createHead();
-extern stackNode* createTail();
-extern stackNode* g_pHead; 
-extern stackNode* g_pTail;
 extern int initializeStack();
 extern void releaseStack();
-extern void releaseNode(stackNode* pNode);
 extern int pushData(char* data);
 extern int popData(char* popedString);
 extern void showAllData();
