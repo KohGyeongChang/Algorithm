a3bd2e864de76eae922cd6b2d8d7670de4f08368
KohGyeongChang <imbrian@icloud.com>
2017-11-19 18:37:52 +0900

    Graph Directed Adjacent Matrix Added

---
+++ Graph_General_Direct/lib/graph_directed_am.h
@@ -0,0 +1,13 @@
+#ifndef GRAPH_DIRECTED_ADJACENTMATRIX_H
+#define GRAPH_DIRECTED_ADJACENTMATRIX_H
+
+
+typedef struct GRAPH_DIRECTED_ADJACENTMATRIX {
+
+	int vertexSize;
+	int** adjacentMatrix;
+
+} graphDAMatrix;
+
+#endif
+
---
+++ Graph_General_Direct/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "./lib/graph_directed_am.h"
+
+extern graphDAMatrix* createGDAM(int);
+extern bool addEdgeGDAM(graphDAMatrix*, int, int);
+extern int getEdgeGDAM(graphDAMatrix*, int, int);
+extern bool removeEdgeGDAM(graphDAMatrix*, int, int);
+extern void deleteGDAM(graphDAMatrix*);
+extern void displayGDAM(graphDAMatrix*);
+
+int main()
+{
+	printf("Graph Directed Adjacent Matrix Test\n");
+	
+	graphDAMatrix* pGraph = createGDAM(10);
+	addEdgeGDAM(pGraph, 0,1);
+	addEdgeGDAM(pGraph, 1,1);
+	addEdgeGDAM(pGraph, 4,1);
+	addEdgeGDAM(pGraph, 0,1);
+	addEdgeGDAM(pGraph, 5,1);
+	addEdgeGDAM(pGraph, 5,7);
+	addEdgeGDAM(pGraph, 7,1);
+	addEdgeGDAM(pGraph, 8,1);
+	addEdgeGDAM(pGraph, 8,3);
+	addEdgeGDAM(pGraph, 8,8);
+	addEdgeGDAM(pGraph, 8,9);
+	addEdgeGDAM(pGraph, 9,9);
+	addEdgeGDAM(pGraph, 10,1);
+	displayGDAM(pGraph);
+	deleteGDAM(pGraph);
+
+	return 0;
+}
