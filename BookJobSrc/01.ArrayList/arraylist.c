2d8284ee0d24a8a4e076fcd41d4789c24d75f26c
KohGyeongChang <imbrian@icloud.com>
2017-12-07 22:36:01 +0900

    Book Job Src : ArrayList Source Start

--- BookJobSrc/01.ArrayList/arraylist.c
+++ BookJobSrc/01.ArrayList/arraylist.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdarg.h>
+
 #include "arraylist.h"
+
+void printErr(const char* pFuncName, char* pErrString)
+{
+	printf("[%s] : %s\n", pFuncName, pErrString);
+}
+
+arrayNode** initializeList(int Size)
+{
+	arrayNode** pArrayList = NULL;
+	pArrayList = (arrayNode**)calloc(1, sizeof(arrayNode*) * Size);
+
+	return pArrayList;
+}
+
+void releaseList(arrayNode** pArrayList, int Size)
+{
+	for(int i = 0; i < Size; ++i) {
+		if ( pArrayList[i] ) {
+			free(pArrayList[i]);
+			pArrayList[i] = NULL;
+		}
+	}
+
+	free(pArrayList);
+
+}
+
+
+bool insertData(arrayNode** pArrayList, int Key, char* pData, int InsertFlag, int InsertPos)
+{
+	if ( (pArrayList == NULL) || (InsertPos < 1 ) || (InsertPos > LISTSIZE) ) {
+		printErr(__func__, 
+		return false;
+	}
+	
+	return true;
+}
+
+
+
--- BookJobSrc/01.ArrayList/arraylist.h
+++ BookJobSrc/01.ArrayList/arraylist.h
@@ -1,7 +1,13 @@
 #ifndef ARRAY_LIST_H
 #define ARRAY_LIST_H
 
+#define LISTSIZE 10
 
+typedef struct ARRAY_NODE {
+	int key;
+	char data[40];
+
+}arrayNode;
 
 
 #endif
