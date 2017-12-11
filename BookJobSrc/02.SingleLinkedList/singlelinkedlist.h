f5fb778670a81d4ece0ce6f4a655411fd89ea1fb
KohGyeongChang <imbrian@icloud.com>
2017-12-11 21:35:39 +0900

    Book Job Src : Single Linked List => Header File Code Changed

--- BookJobSrc/02.SingleLinkedList/singlelinkedlist.h
+++ BookJobSrc/02.SingleLinkedList/singlelinkedlist.h
@@ -1,22 +1,6 @@
 #ifndef SINGLE_LINKED_LIST_H
 #define SINGLE_LINKED_LIST_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/*
-typedef struct NODEDATA {
-	int key;
-	char data[20]; 
-} nodeData;
-
-typedef struct SINGLE_LINKED_LIST {
-	nodeData* pData;
-	struct SINGLE_LINKED_LIST* pNextNode;
-} singleLinkedList;
-*/
-
 typedef struct SINGLE_LINKED_LIST {
 	int key;
 	char data[20]; 
