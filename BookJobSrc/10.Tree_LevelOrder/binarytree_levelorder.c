8cc5b090ae3ee2bf42f85f189a93bdf43902e95f
KohGyeongChang <imbrian@icloud.com>
2017-12-20 20:42:40 +0900

    Book Job Src => Tree LevelOrder Completed

---
+++ BookJobSrc/10.Tree_LevelOrder/binarytree_levelorder.c
@@ -0,0 +1,181 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "binarytree_levelorder.h"
+#include "queue.h"
+
+extern bool initializeQueue(queueNode**, queueNode**);
+extern void releaseQueue(queueNode*);
+extern bool enQueue(queueNode*, treeNode*);	
+extern treeNode* deQueue(queueNode*, queueNode*);
+extern void showAllData(queueNode*);
+extern bool hasQueueData(queueNode*, queueNode*);
+
+treeNode* createTreeNode( int Key, char* pData)
+{
+	treeNode* pTreeNode = NULL;
+
+	pTreeNode = (treeNode*)calloc( 1, sizeof(treeNode) );
+	if ( pTreeNode ) {
+		pTreeNode->key = Key;
+		strcpy(pTreeNode->data, pData );
+		pTreeNode->pLeChild = NULL;
+		pTreeNode->pRiChild = NULL;
+	}
+
+	return pTreeNode;
+}
+
+treeNode* initTree(int Key, char* pData)
+{
+	treeNode* pRoot = createTreeNode(Key, pData);
+	return pRoot;
+}
+
+treeNode* addLeftChild(treeNode* pParentNode, int Key, char* pData )
+{
+	if ( pParentNode == NULL) {
+		printf("Parent Node NULL\n");
+		return NULL;
+	}
+
+	//printf("Parent Node[%s]:Key[%d]\n", pParentNode->data, pParentNode->key);
+
+	treeNode* pTreeNode = NULL;
+
+	pTreeNode = createTreeNode(Key, pData);
+	if ( pTreeNode == NULL ) {
+		printf("Key[%d]:Data[%s] Node Creation FAIL\n", Key, pData);
+		return NULL;
+	}
+
+	if ( pParentNode->pLeChild == NULL ) {
+	//	printf("Left Child Node[%s]:Key[%d]\n\n", pTreeNode->data, pTreeNode->key);
+		pParentNode->pLeChild = pTreeNode;
+	}
+	else {
+		printf("Left Child Already Inserted =>Key[%d]:Data[%s] \n", Key, pData);
+	}
+	return pTreeNode;
+}
+
+
+treeNode* addRightChild(treeNode* pParentNode, int Key, char* pData )
+{
+	if ( pParentNode == NULL) {
+		printf("Parent Node NULL\n");
+		return NULL;
+	}
+
+	//printf("Parent Node[%s]:Key[%d]\n", pParentNode->data, pParentNode->key);
+
+	treeNode* pTreeNode = NULL;
+
+	pTreeNode = createTreeNode(Key, pData);
+	if ( pTreeNode == NULL ) {
+		printf("Key[%d]:Data[%s] Node Creation FAIL\n", Key, pData);
+		return NULL;
+	}
+
+	if ( pParentNode->pRiChild == NULL ) {
+	//	printf("Right Child Node[%s]:Key[%d]\n\n", pTreeNode->data, pTreeNode->key);
+		pParentNode->pRiChild = pTreeNode;
+	}
+	else {
+		printf("Right Child Already Inserted =>Key[%d]:Data[%s] \n", Key, pData);
+	}
+	return pTreeNode;
+}
+
+treeNode* buildBinaryTree()
+{
+	treeNode* pRoot = initTree(0, "A");
+
+	if ( pRoot == NULL ) {
+		printf("Tree Root Init FAIL\n");
+		return NULL;
+	}
+
+	treeNode* pLeft  = NULL;
+    treeNode* pRight = NULL;
+
+    pLeft  = addLeftChild(pRoot, 11, "B");
+
+    if ( pLeft ) {
+		pRight = addRightChild(pLeft, 112, "E");
+
+	    if ( pRight ) {
+
+			addLeftChild(pRight, 1121, "I");
+		    addRightChild(pRight, 1122, "J");
+	    }
+
+	    pLeft  = addLeftChild(pLeft, 111, "D");
+
+	    if ( pLeft ) {
+	        pRight = addRightChild(pLeft,1112, "H");
+	        pLeft  = addLeftChild(pLeft, 1111, "G");
+        }
+
+    }
+
+	pRight = addRightChild(pRoot, 12, "C");
+	if ( pRight ) {
+		pLeft  = addLeftChild(pRight, 121, "F");
+		if( pLeft ) {
+			addRightChild(pLeft, 1212, "L");
+		    addLeftChild(pLeft, 1211, "K");
+		}
+	}
+
+	return pRoot;
+}
+
+void releaseBinaryTree(treeNode* pTreeNode)
+{
+	if ( pTreeNode == NULL ) {
+		return;
+	}
+
+	releaseBinaryTree(pTreeNode->pLeChild);
+	releaseBinaryTree(pTreeNode->pRiChild);
+
+	free(pTreeNode);
+
+}
+
+
+void traverseTree_LevelOrder(treeNode* pTreeNode)
+{
+	printf("\nTree LevelOrder Traverse Test\n");
+
+	queueNode* pHead = NULL;
+	queueNode* pTail = NULL;
+
+	initializeQueue(&pHead, &pTail);
+	enQueue(pTail, pTreeNode);
+
+	while ( 1 ) {
+		if ( hasQueueData( pHead, pTail) ) {
+			pTreeNode = deQueue(pHead, pTail);
+			printf("[%s] -> ", pTreeNode->data);
+
+			if ( pTreeNode->pLeChild != NULL) {
+				enQueue(pTail, pTreeNode->pLeChild);
+			}
+
+			if ( pTreeNode->pRiChild != NULL) {
+				enQueue(pTail, pTreeNode->pRiChild);
+			}
+		}
+		else {
+			break;
+		}
+
+	}
+
+   printf("\n\n");
+   releaseQueue(pHead);
+}
+
---
+++ BookJobSrc/10.Tree_LevelOrder/queue.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "queue.h"
+#include "binarytree_levelorder.h"
+
+queueNode* createNode(treeNode* pTreeNode)
+{
+	queueNode* pNewNode = (queueNode*)calloc(1, sizeof(queueNode));
+	if ( pNewNode == NULL ) {
+		printf("New Node Memory Allocation FAIL\n");
+		return NULL;
+	}
+
+	pNewNode->pTreeNode = pTreeNode;
+	return pNewNode;
+}
+
+queueNode* createHead()
+{
+	// Caller Checks if pointer is NULL
+	queueNode* pHead = createNode(NULL);
+	return pHead;
+}
+
+
+queueNode* createTail()
+{
+	// Caller Checks if pointer is NULL
+	queueNode* pTail= createNode(NULL);
+	return pTail;
+}
+
+bool initializeQueue(queueNode** pHead, queueNode** pTail)
+{
+	*pHead = createHead();
+	if ( *pHead == NULL ) {
+		printf("CreateHead FAIL\n"); 
+		return false;
+	}
+
+	*pTail = createTail();
+	if ( *pTail == NULL ) {
+		printf("CreateTail FAIL\n"); 
+		return false;
+	}
+
+	(*pHead)->pNextNode = *pTail;
+	(*pHead)->pPrevNode = NULL;
+	(*pTail)->pNextNode = NULL;
+	(*pTail)->pPrevNode = (*pHead);
+
+	return true;
+}
+
+void releaseQueue(queueNode* pHead)
+{
+	queueNode* pTmpNode = pHead;	
+
+	while( pTmpNode ) {
+		queueNode* pTmpNextNode = NULL;	
+		if ( pTmpNode->pNextNode ) {
+			pTmpNextNode = pTmpNode->pNextNode;
+		}
+		free(pTmpNode);
+		pTmpNode = pTmpNextNode;
+	}
+}
+
+void releaseNode(queueNode* pNode)
+{
+	if ( pNode == NULL ) {
+		return;
+	}
+
+	free(pNode);
+}
+
+bool hasQueueData(queueNode* pHead, queueNode* pTail)
+{
+	if ( pHead->pNextNode == pTail ) {
+		return false;
+	}
+
+	return true;
+}
+
+bool enQueue(queueNode* pTail, treeNode* pTreeNode)
+{
+	if ( pTail == NULL ) {
+		return false;
+	}
+
+	queueNode* pNewNode= createNode(pTreeNode);
+	if ( pNewNode == NULL ) {
+		printf("Creaete New NODE FAIL\n");
+		return false;
+	}
+
+	pTail->pPrevNode->pNextNode = pNewNode;
+	pNewNode->pNextNode = pTail;
+	pNewNode->pPrevNode = pTail->pPrevNode;
+	pTail->pPrevNode = pNewNode;
+
+	return true;
+
+}
+
+treeNode* deQueue(queueNode* pHead, queueNode* pTail)
+{
+	if ( pHead == NULL ) {
+		return false;
+	}
+
+	if ( pHead->pNextNode == pTail) {
+		printf("No DATA\n");
+		return false;
+	}
+
+	queueNode* pPopedNode = pHead->pNextNode; 
+	pHead->pNextNode = pPopedNode->pNextNode;
+	pPopedNode->pNextNode->pPrevNode = pHead;
+
+	treeNode* pTreeNode = pPopedNode->pTreeNode;
+	free(pPopedNode);
+
+	return pTreeNode;
+}
+
+void showAllData(queueNode* pHead)
+{
+	printf("\n");
+
+	queueNode* pTmpNode = pHead;	
+
+	while( pTmpNode ) {
+		printf("Node Key[%d]:Data[%s]\n", pTmpNode->pTreeNode->key, pTmpNode->pTreeNode->data);
+		queueNode* pTmpNextNode = NULL;	
+		if ( pTmpNode->pNextNode ) {
+			pTmpNextNode = pTmpNode->pNextNode;
+		}
+		pTmpNode = pTmpNextNode;
+	}
+	printf("\n");
+}
+
+
