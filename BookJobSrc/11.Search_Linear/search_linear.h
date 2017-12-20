fe90787bba5ae2fc2d4e0f60f402baf36ef825d6
KohGyeongChang <imbrian@icloud.com>
2017-12-20 20:44:28 +0900

    Book Job Src => Search Linear Completed

---
+++ BookJobSrc/11.Search_Linear/search_linear.h
@@ -0,0 +1,11 @@
+#ifndef LINEAR_SEARCH_H
+#define LINEAR_SEARCH_H
+
+typedef struct SINGLE_LINKED_LIST {
+	int key;
+	char data[20]; 
+	struct SINGLE_LINKED_LIST* pNextNode;
+} singleLinkedList;
+
+
+#endif
