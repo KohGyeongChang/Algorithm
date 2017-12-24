2b41b67dc0655b408b8ab3d461e94f7e11dfa0d5
KohGyeongChang <imbrian@icloud.com>
2017-12-24 16:07:39 +0900

    Book Job Src => Selection Sort Completed

---
+++ BookJobSrc/13.Sort_Selection/selectionSort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "selectionSort.h"
+
+void displayList(int [], int); 
+
+void selectionSort(int inputList[], int inputSize)
+{
+	int indexOfMinValue;	
+	for ( int i = 0 ; i < inputSize; ++i ) {
+		indexOfMinValue = i;
+
+		for ( int j = i + 1; j < inputSize; ++j ) {
+			if ( inputList[j] < inputList[indexOfMinValue] ) {
+				indexOfMinValue = j;
+			}
+
+		}
+
+		int tmp = inputList[i];
+		inputList[i] = inputList[indexOfMinValue];
+		inputList[indexOfMinValue] = tmp;
+		displayList(inputList, inputSize);
+	}
+
+}
+
+void displayList(int inputList[], int inputSize) 
+{
+	for ( int i = 0; i < inputSize; ++i ) {
+		printf("%04d ", inputList[i]);
+	}
+	printf("\n");
+
+}
---
+++ BookJobSrc/13.Sort_Selection/selectionSort.h
@@ -0,0 +1,10 @@
+#ifndef SELECTION_SORT_H
+#define SELECTION_SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#endif
+
